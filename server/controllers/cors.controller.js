52423cfd087054ef87b6ec00025a528307f25da3
Niskarsh Kumar <niskarshk@gmail.com>
2018-08-09 22:32:24 +0530

    Cors headers added

---
+++ server/controllers/cors.controller.js
@@ -0,0 +1,7 @@
+export const authenticate = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Expose-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next()
+}
--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import signin from './signin.routes'
+import { authenticate } from '../controllers/cors.controller'
 
 let router = express.Router()
-
+router.use('/', authenticate)
 router.use ('/signin', signin)
 router.get ('/', (req, res) => {
     res.send ('In backend')
